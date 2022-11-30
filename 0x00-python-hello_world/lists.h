6ae304d6e4bbd52eab29655eff0cbb00f58f30f4
Kendralene <ehikendra93@gmail.com>
2022-11-30 11:46:11 -0800

    Task 10

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -4,11 +4,14 @@
 #include <stdlib.h>
 
 typedef struct listint_s
+
 {
 int n;
-truct listint_s *next;
+struct listint_s *next;
+
 } listint_t;
 size_t print_listint(const listint_t *h);
+
 listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
